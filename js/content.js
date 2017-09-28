2460de731c504ed5841d09257d3c6c88013cbeaa
Traci Potocnik <traci.potocnik@gmail.com>
2017-09-28 15:23:20 -0400

    Moving sizes to external json file

--- js/content.js
+++ js/content.js
@@ -6,71 +6,52 @@
 // OK LETS TRY SOME SHIT
 
 // global variables
-var newAdWidth,
-  newAdHeight,
-  closestAdSize,
-  finalCount;
-
-var sizes = [{
-    "w": 250,
-    "h": 250
-  },
-  {
-    "w": 200,
-    "h": 200
-  },
-  {
-    "w": 468,
-    "h": 60
-  },
-  {
-    "w": 728,
-    "h": 90
-  },
-  {
-    "w": 300,
-    "h": 250
-  },
-  {
-    "w": 336,
-    "h": 280
-  },
-  {
-    "w": 120,
-    "h": 600
-  },
-  {
-    "w": 160,
-    "h": 600
-  },
-  {
-    "w": 970,
-    "h": 90
-  }
-];
+var newAdWidth = null,
+  newAdHeight = null,
+  closestAdSize = null,
+  finalCount = null,
+  sizes = null;
 
 // function that finds the closest ad sizing based on whatever the first ad item blocked is
-function closest(num) {
+function closest(num, sizeArr) {
 
-  var curr = sizes[0].w,
+  var curr = sizeArr[0].w,
     diff = Math.abs(num - curr);
 
-  for (var i = 0; i < sizes.length; i++) {
-    var newdiff = Math.abs(num - sizes[i].w);
+  for (var i = 0; i < sizeArr.length; i++) {
+    var newdiff = Math.abs(num - sizeArr[i].w);
     if (newdiff < diff) {
       diff = newdiff;
-      curr = sizes[i].w;
+      curr = sizeArr[i].w;
     }
   }
 
   return curr;
 }
 
+function getJSONData(url) {
+  var data = null;
+
+  $.ajax({
+    'async': false,
+    url: url,
+    dataType: 'json',
+    success: function(response) { // callback for successful completion
+      data = response;
+    },
+    error: function() { // callback if there's an error
+      // do something
+    }
+  });
+
+  return data;
+}
+
 // LETS CREATE US A NEW ADD
-function createNewAd() {
-  var index = sizes.map(function(ad) { return ad.w; }).indexOf(newAdWidth),
+function createNewAd(client, sizeArr) {
+  var index = sizeArr.map(function(ad) { return ad.w; }).indexOf(newAdWidth),
     finalAsk;
-  newAdHeight = sizes[index].h;
+  newAdHeight = sizeArr[index].h;
   //console.log(newAdWidth + ', ' + newAdHeight);
 
   // Ask limits - if there are more than 25 ads, only ask for $25
@@ -80,16 +61,20 @@ function createNewAd() {
     finalAsk = finalCount;
   }
 
-  var newAd = '<div id="blue-state-ask" style="width:' + newAdWidth + 'px;height:' + newAdHeight + 'px;"><h3>We blocked ' + finalCount + ' ads on this page. Consider donating $' + finalAsk + ' to COMPANY</h3></div>';
+  var newAd = '<div id="blue-state-ask"' +
+    'style="width:' + newAdWidth + 'px; min-height:' + newAdHeight + 'px;">' +
+    '<h3><a href="' + client.url + '">We blocked ' + finalCount + ' ads on this page. Consider donating $' +
+    finalAsk + ' to ' + client.title + '</a></h3></div>';
 
   $('.replaceThisItem').append(newAd).show();
 }
 
 // do all this stuff only when the page finishes loading
 $(document).ready(function() {
-
   var ads = $('.ad, [class*=dfp], [data-google-query-id]'),
     counter = 0;
+
+  sizes = getJSONData(chrome.extension.getURL('data/clients.json'));
 
   ads.each(function() {
 
@@ -100,7 +85,7 @@ $(document).ready(function() {
       // get the width of it
       replaceAdWidth = $(this).width();
       // find the closet ad space
-      newAdWidth = closest(replaceAdWidth);
+      newAdWidth = closest(replaceAdWidth, sizes);
     }
 
     // hide everything
@@ -117,5 +102,7 @@ $(document).ready(function() {
 
 // only create the new ad after the page finishes loading
 $(window).load(function() {
-  createNewAd();
+  var clients = getJSONData(chrome.extension.getURL('data/clients.json'));
+
+  createNewAd(clients[0], sizes);
 });
